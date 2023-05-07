0b33111a5dec3539e6a0c6be7a0abaaae8ae176c
tasita <uatasita@gmail.com>
2023-05-07 23:40:23 +0300

    Initial commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.style = 'list-style: none';
+const imagesMarkup = createGalleryImagesMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+function createGalleryImagesMarkup(el) {
+  return el.reduce((acc, { preview, original, description }) => {
+    return (acc += `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`);
+  }, '');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});  
