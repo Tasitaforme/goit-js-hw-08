2f3754853350b87cf131a88a3ad435495768fc4f
tasita <uatasita@gmail.com>
2023-05-08 05:20:52 +0300

    Initial commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+updateCurrentTimeFromLocalStorage();
+
+player.on("timeupdate", throttle(saveCurrentTimeToLocalStorage, 1000))
+
+function saveCurrentTimeToLocalStorage(e) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, e.seconds || 0);
+};
+
+
+function updateCurrentTimeFromLocalStorage(e) {
+    const savedCurrentTime = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (savedCurrentTime) {
+        player.setCurrentTime(savedCurrentTime)
+    }  
+};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(saveCurrentInputToLocalStorage, 500)
+);
+
+fillFieldsWithSavedUserData();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    
+    console.log(formData)
+    e.currentTarget.reset();
+};
+
+function saveCurrentInputToLocalStorage(e) {
+    formData[e.target.name] = e.target.value;
+    const userData = JSON.stringify(formData);
+    localStorage.setItem(LOCAL_STORAGE_KEY, userData);
+}
+
+function fillFieldsWithSavedUserData() {
+    const isSavedUserData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const savedFormData = JSON.parse(isSavedUserData);
+    
+    if (isSavedUserData) {
+        Object.entries(savedFormData).forEach(([key, value]) => {
+            formEl.elements[key].value = value;
+        });
+    }  
+}
